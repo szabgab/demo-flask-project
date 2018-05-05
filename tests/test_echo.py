87f8c61223dbfd52d7a594604cc3e0a3095eb856
Gabor Szabo <gabor@szabgab.com>
2018-05-05 21:40:51 +0300

    add another test

--- tests/test_echo.py
+++ tests/test_echo.py
@@ -7,10 +7,17 @@ class TestEcho(object):
     def setup_class(self):
         self.app = app.test_client()
 
-    def test_echo(self):
+    def test_main_page(self):
         rv = self.app.get('/')
         assert rv.status == '200 OK'
         assert b'<h1>Hello World!</h1>' in rv.data
         assert b'<form action="/echo" method="POST">' in rv.data
         assert b'You said: ' not in rv.data
+
+    def test_echo(self):
+        rv = self.app.post('/echo', data = { 'text' : 'Foo Bar' })
+        assert rv.status == '200 OK'
+        assert b'<h1>Hello World!</h1>' in rv.data
+        assert b'<form action="/echo" method="POST">' in rv.data
+        assert b'You said: Foo Bar' in rv.data
 
