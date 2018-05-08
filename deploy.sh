aab6837341dbc6b97120b6d7ff0dd8b9588b64b9
Gabor Szabo <gabor@szabgab.com>
2018-05-08 06:37:22 +0200

    fix test

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 echo "Deploy!"
+echo "GIT_COMMIT $GIT_COMMIT"
+echo "GIT_BRANCH $GIT_BRANCH"
+echo "GIT_AUTHOR_NAME $GIT_AUTHOR_NAME"
+echo "GIT_AUTHOR_EMAIL $GIT_AUTHOR_EMAIL"
+
 
 cd /home/gabor/work/demo-flask-project;
 /usr/bin/git pull
