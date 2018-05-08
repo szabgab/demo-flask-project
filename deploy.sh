39cdc0ae16815ed02ef80c5d2f55b323c863169e
Gabor Szabo <gabor@szabgab.com>
2018-05-08 06:47:41 +0200

    more output

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,12 @@
-#!/bin/bash
+#!/bin/bash -xs
 echo "Deploy!"
 echo "GIT_COMMIT $GIT_COMMIT"
 echo "GIT_BRANCH $GIT_BRANCH"
 echo "GIT_AUTHOR_NAME $GIT_AUTHOR_NAME"
 echo "GIT_AUTHOR_EMAIL $GIT_AUTHOR_EMAIL"
+
+echo "GIT_COMMITTER_NAME $GIT_COMMITTER_NAME"
+echo "GIT_COMMITTER_EMAIL $GIT_COMMITTER_EMAIL"
 
 
 cd /home/gabor/work/demo-flask-project;
