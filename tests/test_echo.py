b4123f51a03f220bcb27e56038b6aea56aaa9644
Gabor Szabo <gabor@szabgab.com>
2018-05-29 17:06:53 +0300

    v6

--- tests/test_echo.py
+++ tests/test_echo.py
@@ -21,8 +21,8 @@ class TestEcho(object):
         assert b'<form action="/echo" method="POST">' in rv.data
         assert b'You said: Foo Bar' in rv.data
 
-#    def test_demo(self):
-#        assert False
+    def test_demo(self):
+        assert False
 
     def test_more(self):
         assert True
